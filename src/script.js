12c5fe81a2aee8081bee4b6877699585a6b05a80
Davide0000 <92744664+Davide0000@users.noreply.github.com>
2022-07-14 11:12:43 +0200

    added local storage to save dark mode preference

--- src/script.js
+++ src/script.js
@@ -1,8 +1,20 @@
-function darkmode(){
-   var element = document.body;
-   element.classList.toggle("darkmode");
 
-   const grid = document.getElementById('tweetcards');
+if(localStorage.getItem('darkMode') === null) localStorage.setItem('darkMode','false');
+loadMode();
+
+function loadMode(){
+   if(localStorage.getItem('darkMode') === 'true'){
+      document.getElementById('darkmode-checkbox').checked = true;
+      toggleStyle();
+      
+   }
+}
+
+function toggleStyle(){
+   var element = document.body;
+   element.classList.toggle('darkmode');
+
+   const grid = document.getElementById('tweet-cards');
    const cards = grid.querySelectorAll('.uk-card-body');
    cards.forEach((card)=> {
     card.classList.toggle("uk-card-secondary");
@@ -23,10 +35,19 @@ function darkmode(){
 
    selectedCategory.classList.toggle("uk-button-secondary");
 
-   const divButtons = document.getElementById('classifierbuttons');
+   const divButtons = document.getElementById('classifier-buttons');
    const buttons = divButtons.querySelectorAll('.uk-button');
    buttons.forEach((button)=>{
     button.classList.toggle("uk-button-secondary");
 
    })
 }
+document.getElementById('darkmode-checkbox').addEventListener('click',() => {
+   if(localStorage.getItem('darkMode') === 'false'){
+      localStorage.setItem('darkMode','true');
+   }else{
+      localStorage.setItem('darkMode','false');
+   }
+   toggleStyle();
+}) 
+   
